43152e4a862891576a3a643e9a07efdb8e7c846d
Romark Fernandez [SPXPH4227] <romark.fernandez@spxexpress.com>
2026-06-28 20:05:04 +0800

    aws

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -4,11 +4,13 @@ use App\Http\Middleware\AuthenticateSupabase;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Routing\Middleware\ThrottleRequests;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(api: __DIR__.'/../routes/api.php', health: '/up')
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias(['supabase.auth' => AuthenticateSupabase::class]);
+        $middleware->prependToPriorityList(ThrottleRequests::class, AuthenticateSupabase::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {})
     ->create();
