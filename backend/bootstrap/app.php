5b721d2d06c51247984d64a31eaa284d5802aebb
Romark Fernandez [SPXPH4227] <romark.fernandez@spxexpress.com>
2026-06-28 23:42:49 +0800

    sentry

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Routing\Middleware\ThrottleRequests;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(api: __DIR__.'/../routes/api.php', health: '/up')
@@ -12,5 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias(['supabase.auth' => AuthenticateSupabase::class]);
         $middleware->prependToPriorityList(ThrottleRequests::class, AuthenticateSupabase::class);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {})
+    ->withExceptions(function (Exceptions $exceptions): void {
+        Integration::handles($exceptions);
+    })
     ->create();
