a7ee2ce6c68f382223be8a6378009ec12619295e
Romark Fernandez [SPXPH4227] <romark.fernandez@spxexpress.com>
2026-06-28 22:55:55 +0800

    Fix Laravel bootstrap without APP_URL

--- backend/config/app.php
+++ backend/config/app.php
@@ -1,3 +1,3 @@
 <?php
 
-return ['name' => env('APP_NAME', 'SOC 5 Outbound'), 'env' => env('APP_ENV', 'production'), 'debug' => (bool) env('APP_DEBUG', false), 'url' => env('APP_URL'), 'timezone' => 'Asia/Manila', 'key' => env('APP_KEY'), 'cipher' => 'AES-256-CBC'];
+return ['name' => env('APP_NAME', 'SOC 5 Outbound'), 'env' => env('APP_ENV', 'production'), 'debug' => (bool) env('APP_DEBUG', false), 'url' => env('APP_URL', 'http://localhost'), 'timezone' => 'Asia/Manila', 'key' => env('APP_KEY'), 'cipher' => 'AES-256-CBC'];
